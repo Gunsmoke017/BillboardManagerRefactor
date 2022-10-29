41a99a8144f267f4376a5a12a7b9616bc2eebaeb
Gunsmoke017 <Percivalrp106@gmail.com>
2022-10-29 23:53:10 +0100

    Prepared admin methods interface

---
+++ src/main/java/interfaces/IAdminMethods.java
@@ -0,0 +1,14 @@
+package interfaces;
+
+import entity.billboards.Billboard;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IAdminMethods {
+    boolean registerNewBillboard(Billboard billboard);
+    List<Billboard> viewAllBillboards();
+    Optional<Billboard> viewBillboardByLocation(String location);
+    Billboard viewBillboardById(long serialNumber);
+    String takeDownAd(long serialNumber, char confirm);
+}
