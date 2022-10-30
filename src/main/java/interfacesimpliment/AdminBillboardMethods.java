fcf4d8505de80e8d95a8750eab6e745fa0d0eab4
Gunsmoke017 <Percivalrp106@gmail.com>
2022-10-30 09:34:27 +0100

    Prepared view all method

--- src/main/java/interfacesimpliment/AdminBillboardMethods.java
+++ src/main/java/interfacesimpliment/AdminBillboardMethods.java
@@ -2,11 +2,14 @@ package interfacesimpliment;
 
 import database.BillboardDb;
 import entity.billboards.Billboard;
+import entity.billboards.BookingDetails;
+import enums.State;
 import interfaces.IAdminBillboardMethods;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -45,7 +48,34 @@ public class AdminBillboardMethods implements IAdminBillboardMethods {
 
     @Override
     public List<Billboard> viewAllBillboards() {
-        return null;
+        ArrayList<Billboard> billboardsHolder = new ArrayList<>();
+        String SEARCH = "SELECT * FROM billboardsdb";
+        if(billboardDb.connectToBillboardDb()){
+            try{
+                preparedStatement = billboardDb.getConnections().prepareStatement(SEARCH);
+                resultSet = preparedStatement.executeQuery();
+                while(resultSet.next()){
+                    Billboard billboard = new Billboard();
+                    BookingDetails bookingDetails = new BookingDetails();
+                    billboard.setSerialNumber(resultSet.getInt("serialnumber"));
+                    billboard.setLocation(resultSet.getString("location"));
+                    billboard.setDimension(resultSet.getString("dimension"));
+                    billboard.setState(State.valueOf(resultSet.getString("state")));
+                    billboard.setPricePerHr((resultSet.getInt("priceperhr")));
+                    bookingDetails.setCustomer(resultSet.getString("customer"));
+                    bookingDetails.setBookedDate(resultSet.getString("bookeddate"));
+                    bookingDetails.setTimeBooked(resultSet.getString("timebooked"));
+                    bookingDetails.setUploadedFile(resultSet.getString("uploadedfile"));
+                    bookingDetails.setDurationOfBooking(resultSet.getInt("duration"));
+                    billboard.setBookingDetails(bookingDetails);
+                    billboardsHolder.add(billboard);
+                }
+            }
+            catch(SQLException ee){
+                ee.printStackTrace();
+            }
+        }
+        return billboardsHolder;
     }
 
     @Override
