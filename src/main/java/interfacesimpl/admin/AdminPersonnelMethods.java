f1787e035c2e0bb7919e1303e3a3367fcd8c35c7
Gunsmoke017 <Percivalrp106@gmail.com>
2022-11-19 16:57:19 +0100

    Finshed merthod for registering personnel

--- src/main/java/interfacesimpl/admin/AdminPersonnelMethods.java
+++ src/main/java/interfacesimpl/admin/AdminPersonnelMethods.java
@@ -1,14 +1,44 @@
 package interfacesimpl.admin;
 
+import database.BillboardDb;
 import entity.person.Personnel;
 import interfaces.admin.IAdminPersonnelMethods;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class AdminPersonnelMethods implements IAdminPersonnelMethods {
+    BillboardDb billboardDb = new BillboardDb();
+    PreparedStatement preparedStatement;
+    ResultSet resultSet;
+
     @Override
     public boolean registerNewAdmin(Personnel personnel) {
-        return false;
+        boolean outcome = false;
+        int upd;
+        String INSERT = "INSERT INTO admin (email, firstname, lastname, password) VALUES (?,?,?,?)";
+        if(billboardDb.connectToBillboardDb()) {
+            try{
+                preparedStatement = billboardDb.getConnections().prepareStatement(INSERT);
+                preparedStatement.setString(1, personnel.getEmail());
+                preparedStatement.setString(2, personnel.getFirstName());
+                preparedStatement.setString(3, personnel.getLastName());
+                preparedStatement.setString(4,personnel.getPassword());
+                upd = preparedStatement.executeUpdate();
+
+                if(upd == 0){
+//                    Add exception for failure to insert into database
+                } else{
+                    System.out.println(" >> Personnel registered successfully");
+                    outcome = true;
+                }
+            } catch(SQLException e){
+                e.printStackTrace();
+            }
+        }
+        return outcome;
     }
 
     @Override
